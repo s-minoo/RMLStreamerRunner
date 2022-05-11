932007325d1faf58db6b29aa8bdb1092db819d9a
Sitt Min Oo <sittminoo1995145@gmail.com>
2022-05-11 10:05:01 +0200

    added namespaces

---
+++ app/src/main/scala/RMLRunner/vocabulary/RMLSVoc.scala
@@ -0,0 +1,38 @@
+package RMLRunner.vocabulary
+
+object RMLSVoc {
+
+  val namespace = ("rmls", "http://semweb.mmlab.be/ns/rmls#")
+
+  object Property {
+    ///////////////////////////////////////////////////////////////////////////
+    // RMLS: TCP Source
+    ///////////////////////////////////////////////////////////////////////////
+
+    val HOSTNAME = namespace._2 + "hostName";
+    val PORT = namespace._2 + "port";
+    val PATH = namespace._2 + "path";
+    val TYPE = namespace._2 + "type";
+
+    ///////////////////////////////////////////////////////////////////////////
+    // RMLS: Kafka Source
+    ///////////////////////////////////////////////////////////////////////////
+
+    val BROKER = namespace._2 + "broker";
+    val GROUPID = namespace._2 + "groupId";
+    val TOPIC = namespace._2 + "topic";
+    val KAFKAVERSION = namespace._2 + "kafkaVersion";
+
+
+  }
+
+  object Class {
+
+    val KAFKA_STREAM = namespace._2 + "KafkaStream"
+    val TCP_STREAM = namespace._2 + "TcpStream"
+  }
+
+  def apply(suffix:String):String = {
+    namespace._2 +  suffix 
+  }
+}
---
+++ app/src/main/scala/RMLRunner/vocabulary/RMLVoc.scala
@@ -0,0 +1,24 @@
+package RMLRunner.vocabulary
+
+object RMLVoc {
+  val namespace = ("rml", "http://semweb.mmlab.be/ns/rml#");
+
+  object Property {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // RML
+    ///////////////////////////////////////////////////////////////////////////
+    val REFERENCE = namespace._2 + "reference";
+    val LOGICALSOURCE = namespace._2 + "logicalSource";
+    val ITERATOR = namespace._2 + "iterator";
+    val REFERENCEFORMULATION = namespace._2 + "referenceFormulation";
+    val SOURCE = namespace._2 + "source";
+    val LOGICALTARGET = namespace._2 + "logicalTarget";
+  }
+
+
+
+  def apply(suffix:String):String = {
+    namespace._2 +  suffix 
+  }
+}
